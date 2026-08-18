95a9e692a087cac3bd963c485889f133828164fa
hrit24-arch <hrit2005hrit@gmail.com>
2026-08-18 22:30:19 +0530

    Update qr.py

--- qr.py
+++ qr.py
@@ -1,8 +1,15 @@
 import qrcode
 import secrets
+import os
 secure_otp=f"{secrets.randbelow(1000000):06d}"
 print(secure_otp)
-file_path="D:\\quizqr\\qr.png"
+file_path="D:\\quizqr"
+counter = 1
+
+while os.path.exists(os.path.join(file_path, f"qr_{counter}.png")):
+    counter += 1
+
+file_path = os.path.join(file_path, f"qr_{counter}.png")
 qr=qrcode.QRCode()
 qr.add_data(secure_otp)
 img=qr.make_image()
